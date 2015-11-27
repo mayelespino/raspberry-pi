fb6a93f4b6c3a17337725d2b88c803bb0a933638
mayelespino <mayel.espino@gmail.com>
2015-11-27 07:00:27 +0000

    clean up and adding setting local time.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # raspberry-pi
 raspberry pie projects
+======
+These are my project for the raspberry pie
--- clock/clock.py
+++ clock/clock.py
@@ -1,13 +1,21 @@
 import time
-import os
+from time import ctime
+import datetime
 
+clock_time = None
 try:
     import ntplib
     server = 'pool.ntp.org'
     client = ntplib.NTPClient()
     response = client.request(server)
-    os.system('date' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time)))
+    localTime = response.tx_time
+    localTime = localTime - 28800 #28800 is 8 hours
+    print ctime(localTime)
+    print time.localtime(localTime)
+    #sudodate = subprocess.Popen(["sudo", "date", "-s", "Thu Aug  9 21:31:26 UTC 2012"])
+    #sudodate.communicate()
 except:
   print "Could not synch with server", server 
-
+#except:
+#  print "Error formating time."
 print "Done"
