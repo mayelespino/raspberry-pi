1bcd6d89be09f4e03c9854bd3c673625793930c2
mayelespino <mayel.espino@gmail.com>
2015-11-28 23:37:48 +0000

    v1

--- clock/editCrontab.py
+++ clock/editCrontab.py
@@ -1,8 +1,10 @@
 from crontab import CronTab
  
-tab = CronTab(user='pi',fake_tab='True')
+#tab = CronTab(user='pi',fake_tab='True')
+tab = CronTab(user='pi')
 cmd = '/usr/bin/python /home/pi/DEV/raspberry-pi/clock/playMP3.py'
 cron_job = tab.new(cmd)
-cron_job.minute().every(5)
+#cron_job.minute().every(15)
+cron_job.minute(15)
 tab.write()
 print tab.render()
--- clock/playMP3.py
+++ clock/playMP3.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load("BBT.mp3")
+pygame.mixer.music.load("mp3s/BBT.mp3")
 pygame.mixer.music.set_volume(1.0)
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
