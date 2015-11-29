f78aaa10fb1cfb568d0e85ef1aaf219f955dcf4e
mayelespino <mayel.espino@gmail.com>
2015-11-29 00:15:40 +0000

    v1

--- clock/editCrontab.py
+++ clock/editCrontab.py
@@ -4,7 +4,6 @@ from crontab import CronTab
 tab = CronTab(user='pi')
 cmd = '/usr/bin/python /home/pi/DEV/raspberry-pi/clock/playMP3.py'
 cron_job = tab.new(cmd)
-#cron_job.minute().every(15)
-cron_job.minute(15)
+cron_job.setall('15 * * * *')
 tab.write()
 print tab.render()
