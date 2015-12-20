d81a20c06873ca80e22be78295788c8fecf54ebc
mayelespino <mayel.espino@gmail.com>
2015-12-19 22:07:46 -0800

    added hour.sh

---
+++ clock/hour.sh
@@ -0,0 +1,4 @@
+/home/pi/clock/play.sh Mystic_Chanting_4-Marianne_Gagnon-591053853.mp3
+/home/pi/clock/say.sh "$1"
+/home/pi/clock/say.sh "$1"
+/home/pi/clock/play.sh Mystic_Chanting_4-Marianne_Gagnon-591053853.mp3
