bcf7e9f0cf3fdb1e2f3fe2d191583901e8a76e32
Mayel Espino <mayel.espino@gmail.com>
2016-01-27 20:35:27 -0800

    added hour from date command.

--- clock/hour.sh
+++ clock/hour.sh
@@ -1,4 +1,3 @@
 /home/pi/clock/play.sh Mystic_Chanting_4-Marianne_Gagnon-591053853.mp3
-/home/pi/clock/say.sh "$1"
-/home/pi/clock/say.sh "$1"
+/home/pi/clock/say.sh `date +"%r"`
 /home/pi/clock/play.sh Mystic_Chanting_4-Marianne_Gagnon-591053853.mp3
