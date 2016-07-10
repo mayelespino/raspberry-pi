c518343f6160ad2c25041f39d364d1ed1761f852
Mayel Espino <mayel.espino@gmail.com>
2016-07-09 22:32:25 -0700

    v1

---
+++ clock3/alarm.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/omxplayer /var/www/mp3s/"$1" > /dev/null
---
+++ clock3/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo apt-get install python-pip
+sudo apt-get install python-dev
+sudo apt-get install build-essential
+sudo apt-get install flask
+sudo pip install flask
+sudo apt-get install omxplayer
+#
+sudo mkdir -p /var/www/app
+sudo cp -R app /var/www/app
+sudo chmod -R 775 /var/www/app
+sudo mkdir -p /var/www/mp3
+sudo chmod 775 /var/www/mp3 
+#
+sudp cp run.py /var/www/
+sudo chmod +x /var/www/run.py
+sudo touch /var/www/alarms.txt
+sudo chmod 755 /var/www/alarms.txt
+#
+sudo cp {run.py,alarm.sh,resync.sh} /var/www/
+sudo chmod +x /var/www/{run.py,alarm.sh,resync.sh}
+
+
