8c8f43d71d20e678925de717bb0006b335ce8802
Mayel Espino <mayel.espino@gmail.com>
2016-07-09 23:08:52 -0700

    v1.1

--- clock3/install.sh
+++ clock3/install.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get update
 sudo apt-get install -y apache2
-sudo apt-get install python-pip
-sudo apt-get install python-dev
-sudo apt-get install build-essential
-sudo apt-get install flask
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y flask
 sudo pip install flask
 sudo apt-get install omxplayer
 #
@@ -15,7 +15,7 @@ sudo chmod -R 775 /var/www/app
 sudo mkdir -p /var/www/mp3
 sudo chmod 775 /var/www/mp3 
 #
-sudp cp run.py /var/www/
+sudo cp run.py /var/www/
 sudo chmod +x /var/www/run.py
 sudo touch /var/www/alarms.txt
 sudo chmod 755 /var/www/alarms.txt
