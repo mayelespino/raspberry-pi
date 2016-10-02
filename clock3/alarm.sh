77062f3e6992449710ce299fca5d2a879ae1eb1c
Mayel Espino <mayel.espino@gmail.com>
2016-10-01 20:30:45 -0700

    update alarm

--- clock3/alarm.sh
+++ clock3/alarm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 song=`ls -1 /var/www/mp3s/wakeup | xargs shuf -n1 -e`
-/usr/bin/omxplayer /var/www/mp3s/wakeup/${song} > /dev/null
+/usr/bin/omxplayer /var/www/mp3/wakeup/${song} > /dev/null
