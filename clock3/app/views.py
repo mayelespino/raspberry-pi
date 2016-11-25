a9766d740278d43968427371b9791210cb1fee93
Mayel Espino <mayel.espino@gmail.com>
2016-11-24 23:00:18 -0800

    updated.

--- clock3/app/views.py
+++ clock3/app/views.py
@@ -19,81 +19,63 @@ def index():
     timeString = '[{}/{}/{} {}:{} {}]\n'.format(now.day,now.month,now.year,hour,now.minute,ampm)
     return timeString
 
-@app.route('/alarms/', methods=['GET'])
+@app.route('/cron/', methods=['GET'])
 def doGet():
-    with open ("/var/www/html/alarms.txt", "r") as myfile:
-        data="".join(line for line in myfile)
-    return data
-
-@app.route('/sleep/<time>', methods=['POST'])
-def addSleepDefault(time):
     cron  = CronTab(user=True)
-    job = cron.new(command='/var/www/sleep.sh')
-    job.dow.on(1,2,3,4,5)
-    job.hour.on(time)
-    cron.write()
-    cron.write('/var/www/html/cron.txt')
-    return time+"\n"
+    for line in cron.lines:
+      print line
+    return
 
-@app.route('/alarms/<time>', methods=['POST'])
-def addAlarmDefault(time):
-    writeCronFile(time, None, "alarm")
-    reloadCron()
-    return time+"\n"
-
-@app.route('/alarms/<time>/<days>', methods=['POST'])
-def addAlarmParams(time, days, script="alarm"):
-    writeCronFile(time,days, "sleep")
-    reloadCron()
-    return time+" "+days+"\n"
-
-@app.route('/sleep/<time>/<days>', methods=['POST'])
-def addSleepParams(time, days, script="sleep"):
-    writeCronFile(time,days, "sleep")
-    return time+" "+days+"\n"
-    reloadCron()
-def writeCronFile(time, days=None, script="alarm"):
-    if ':' in time:
-        (hour, minute) = time.split(":")
-    else:
-        hour = time
-        minute = "00"
-
-    if days is None:
-        days = "*"
-
-    if minute is None:
-        minute = "*"
-
-    if script == 'sleep':
-        line = "{} {} * * {} /var/www/sleep.sh\n".format(minute,hour,days)
-    else:
-        line = "{} {} * * {} /var/www/alarm.sh\n".format(minute,hour,days)
-
-    with open ("/var/www/html/alarms.txt", "a") as myfile:
-        myfile.write(line)
-
-@app.route('/alarms/', methods=['DELETE'])
-def clearAlarms():
-    call(["/usr/bin/crontab", "-r"])
-    with open ("/var/www/html/alarms.txt", "w") as myfile:
-        cronFile = """@reboot /var/www/resync.sh\n@reboot /var/www/reload.sh\n# m h  dom mon dow   command\n"""
-        myfile.write(cronFile)
-    reloadCron()
+@app.route('/cron/', methods=['DELETE'])
+def clearCron():
+    cron  = CronTab(user=True)
+    cron.remove_all()
     return "Alarms cleared!\n"
 
-@app.route('/alarms/reload/', methods=['POST'])
-def reloadCron():
-    call(["/usr/bin/crontab", "-r"])
-    call(["/usr/bin/crontab", "/var/www/html/alarms.txt"])
-    return "alarm crontab reloaded\n"
+@app.route('/sleep/daily/<time>', methods=['POST'])
+def sleepDaily(time):
+    cron  = CronTab(user=True)
+    cron_job = cron.new(command='/var/www/sleep.sh')
+    cron_job.enable()
+    cron_job.hour.on(time)
+    cron.write()
+    return "sleep daily at"+time+" added.\n"
 
-@app.route('/sleep/playnow/', methods=['POST'])
-def playSleepNow():
+@app.route('/sleep/weekday/<time>', methods=['POST'])
+def sleepWeekday(time):
+    cron  = CronTab(user=True)
+    cron_job = cron.new(command='/var/www/sleep.sh')
+    cron_job.dow.on(1,2,3,4,5)
+    cron_job.hour.on(time)
+    cron_job.enable()
+    cron.write()
+    return "sleep weekday at"+time+" added.\n"
+
+@app.route('/sleep/now/', methods=['POST'])
+def sleepNow():
     call(["/var/www/sleep.sh"])
     return "sleep playing now.\n"
 
-@app.route('/alarms/playnow/', methods=['POST'])
+@app.route('/wakeup/daily/<time>', methods=['POST'])
+def wakeupDaily(time):
+    cron  = CronTab(user=True)
+    cron_job = cron.new(command='/var/www/wakeup.sh')
+    cron_job.enable()
+    cron_job.hour.on(time)
+    cron.write()
+    return "done\n"
+
+@app.route('/wakeup/weekday/<time>', methods=['POST'])
+def wakeupWeekday(time):
+    cron  = CronTab(user=True)
+    cron_job = cron.new(command='/var/www/wakeup.sh')
+    cron_job.dow.on(1,2,3,4,5)
+    cron_job.hour.on(time)
+    cron_job.enable()
+    cron.write()
+    return "done\n"
+
+@app.route('/wakeup/now/', methods=['POST'])
 def playAlarmNow():
     call(["/var/www/alarm.sh"])
     return "alarm playing now.\n"
