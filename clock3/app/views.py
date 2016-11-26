e40a81d895cbb9b944bc4a1d724f52770824a275
Mayel Espino <mayel.espino@gmail.com>
2016-11-25 22:15:50 -0800

    update

--- clock3/app/views.py
+++ clock3/app/views.py
@@ -5,6 +5,10 @@ from flask import request, session
 from subprocess import call
 from crontab import CronTab
 
+
+#
+# Common
+#
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,7 +23,7 @@ def index():
     timeString = '[{}/{}/{} {}:{} {}]\n'.format(now.day,now.month,now.year,hour,now.minute,ampm)
     return timeString
 
-@app.route('/cron/', methods=['GET'])
+@app.route('/cron', methods=['GET'])
 def doGet():
     cron  = CronTab(user=True)
     returnString = ""
@@ -29,10 +33,22 @@ def doGet():
 
 @app.route('/cron/', methods=['DELETE'])
 def clearCron():
+    call(["crontab", "-r"])
     cron  = CronTab(user=True)
-    cron.remove_all()
+    cron_job = cron.new("/var/www/resync.sh")
+    cron_job.every_reboot()
+    cron_job.enable()
+    cron.write()
     return "Alarms cleared!\n"
 
+@app.route('/mute', methods=['POST'])
+def mute(time):
+    call(["killall", "/usr/bin/omxplayer.bin"])
+    return "sleep playing now.\n"
+
+#
+# Sleep
+#
 @app.route('/sleep/daily/<time>', methods=['POST'])
 def sleepDaily(time):
     if ':' in time:
@@ -40,14 +56,13 @@ def sleepDaily(time):
     else:
         hour = time
         minute = "00"
-
     cron  = CronTab(user=True)
     cron_job = cron.new(command='/var/www/sleep.sh')
-    cron_job.enable()
     cron_job.hour.on(hour)
     cron_job.minute.on(minute)
+    cron_job.enable()
     cron.write()
-    return "sleep daily at"+time+" added.\n"
+    return "sleep daily at ["+time+"] added.\n"
 
 @app.route('/sleep/weekday/<time>', methods=['POST'])
 def sleepWeekday(time):
@@ -56,20 +71,23 @@ def sleepWeekday(time):
     else:
         hour = time
         minute = "00"
-
     cron  = CronTab(user=True)
     cron_job = cron.new(command='/var/www/sleep.sh')
     cron_job.dow.on(1,2,3,4,5)
-    cron_job.hour.on(time)
+    cron_job.hour.on(hour)
+    cron_job.minute.on(minute)
     cron_job.enable()
     cron.write()
-    return "sleep weekday at"+time+" added.\n"
+    return "sleep weekday at ["+time+"] added.\n"
 
 @app.route('/sleep/now/', methods=['POST'])
 def sleepNow():
     call(["/var/www/sleep.sh"])
     return "sleep playing now.\n"
 
+#
+# Wake Up
+#
 @app.route('/wakeup/daily/<time>', methods=['POST'])
 def wakeupDaily(time):
     if ':' in time:
@@ -77,13 +95,13 @@ def wakeupDaily(time):
     else:
         hour = time
         minute = "00"
-
     cron  = CronTab(user=True)
     cron_job = cron.new(command='/var/www/wakeup.sh')
+    cron_job.hour.on(hour)
+    cron_job.minute.on(minute)
     cron_job.enable()
-    cron_job.hour.on(time)
     cron.write()
-    return "done\n"
+    return "wakeup daily at ["+time+"] added.\n"
 
 @app.route('/wakeup/weekday/<time>', methods=['POST'])
 def wakeupWeekday(time):
@@ -92,17 +110,18 @@ def wakeupWeekday(time):
     else:
         hour = time
         minute = "00"
-
     cron  = CronTab(user=True)
-    cron_job = cron.new(command='/var/www/wakeup.sh')
+    cron_job = cron.new(command='/var/www/sleep.sh')
     cron_job.dow.on(1,2,3,4,5)
-    cron_job.hour.on(time)
+    cron_job.hour.on(hour)
+    cron_job.minute.on(minute)
     cron_job.enable()
     cron.write()
-    return "done\n"
+    return "wakeup weekdays at ["+time+"] added.\n"
 
 @app.route('/wakeup/now/', methods=['POST'])
-def playAlarmNow():
-    call(["/var/www/alarm.sh"])
+def wakeupNow():
+    call(["/var/www/wakeup.sh"])
     return "alarm playing now.\n"
-#
+
+# EOF
--- clock3/resync.sh
+++ clock3/resync.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+DTZ="US/Pacific"
 
-sudo service ntpd stop
-sudo ntpd -gq
-sudo service ntpd start
+/etc/init.d/ntp restart
+
+grep "$DTZ" /etc/timezone >/dev/null
+if [ "$?" -ne "0" ]
+then
+  echo "Reseting to ${DTZ}"
+  echo "${DTZ}" > /etc/timezone
+fi
+#
+# http://raspberrypi.stackexchange.com/questions/4370/where-does-the-raspberry-pi-get-the-time-from
+# http://www.pool.ntp.org/en/use.html
+#
+# Replace the list of servers with the one you found in the webpage. e.g.
+#
+# vi /etc/ntp.conf
+#server 0.pool.ntp.org
+#server 1.pool.ntp.org
+#server 2.pool.ntp.org
+#server 3.pool.ntp.org
+#
