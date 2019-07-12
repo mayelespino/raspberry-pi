023fe5f0240b4aee14fb73a5b0c41e402be22af2
mayel.espino@gmail.com <mayel.espino@gmail.com>
2019-07-11 21:39:53 -0700

    added ngrok to startApp.sh

--- picron/www/startApp.sh
+++ picron/www/startApp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /var/www/
-nohup /usr/bin/python /var/www/run.py  &
+nohup /usr/bin/python /var/www/run.py > /dev/null 2>&1&
 ps aux | grep run.py |grep -v grep| awk '{print $2;}' > /var/run/picron.pid
+nohup ngrok start -config=/var/www/ngrok.yml --all > /dev/null 2>&1&
