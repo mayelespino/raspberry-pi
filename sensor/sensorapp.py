de17120b3d0bea4c9b55180b53d207b5f0342bfb
mayelespino <mayel.espino@gmail.com>
2019-11-20 18:20:53 -0800

    added writting of sensor files. updated Ansible playbook.

--- sensor/sensorapp.py
+++ sensor/sensorapp.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'OK'
+
+@app.route('/human/')
+def human_sensor():
+    with open('/mnt/ramdisk/sensor-human.txt','r') as f:
+        data = f.read()
+    return data
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
