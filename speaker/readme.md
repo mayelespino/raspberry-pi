c6352cbc574e811b82cf7dd5bad2ff60e487e826
mayelespino <mayel.espino@gmail.com>
2019-11-25 02:49:08 +0000

    Update sounds

--- speaker/readme.md
+++ speaker/readme.md
@@ -9,6 +9,15 @@
   - http://192.240.102.198:14244/listen.pls
   - http://5.39.71.159:8157/listen.pls
   - http://94.23.252.14:8067/listen.pls
+- bible
+  - http://66.55.145.43:8018/listen.pls
+  - http://64.71.79.181:6552/listen.pls
+- bibla
+  - http://94.23.6.53:8358/listen.pls
+- birds
+  - http://209.126.119.76:8200/listen.pls
+- ocean
+  - http://5.39.71.159:8157/listen.pls
 
 # Volume control
 - amixer set PCM -- 100%
--- speaker/speakerapp.py
+++ speaker/speakerapp.py
@@ -29,5 +29,40 @@ def play_pink():
     subprocess.Popen(["/usr/bin/mplayer","-playlist","http://uk1.internet-radio.com:8004/listen.pls"])
     return "OK"
 
+@app.route('/bible/', methods=['POST'])
+def play_bible():
+    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://66.55.145.43:8018/listen.pls"])
+    return "OK"
+
+@app.route('/biblia/', methods=['POST'])
+def play_bibla():
+    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://94.23.6.53:8358/listen.pls"])
+    return "OK"
+
+@app.route('/birds/', methods=['POST'])
+def play_birds():
+    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://209.126.119.76:8200/listen.pls"])
+    return "OK"
+
+@app.route('/ocean/', methods=['POST'])
+def play_ocean():
+    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://5.39.71.159:8157/listen.pls"])
+    return "OK"
+
+@app.route('/100/', methods=['POST'])
+def vol_100():
+    subprocess.Popen(["sudo","/usr/bin/amixer","set", "PCM", "--", "100%"])
+    return "OK"
+
+@app.route('/75/', methods=['POST'])
+def vol_75():
+    subprocess.Popen(["sudo","/usr/bin/amixer","set", "PCM", "--", "75%"])
+    return "OK"
+
+@app.route('/50/', methods=['POST'])
+def vol_50():
+    subprocess.Popen(["sudo","/usr/bin/amixer","set", "PCM", "--", "50%"])
+    return "OK"
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
