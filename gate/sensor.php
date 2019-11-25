87c2c28e11407efea8d2a150b6856859e226b441
mayelespino <mayel.espino@gmail.com>
2019-11-25 03:46:53 +0000

    Updated all gate PHP files.

--- gate/sensor.php
+++ gate/sensor.php
@@ -4,61 +4,91 @@
 <body>
 
 <h1>17440 Holiday Drive</h1>
+
+<a href="index.php">[HOME]</a>
+
 <h2>Ambiant sensors</h2>
+<br/>
+
+<table>
+<tr>
+<td>
+<h3>Last check<h3/>
 <b>
 <?php
 $homepage = file_get_contents('http://sensor.local:5000/time-stamp/');
 echo $homepage;
 ?>
-</b>
-<br/>
 
-<h2>Brightness</h2>
+<td/>
+<td>
+
+<h3>Brightness</h3>
 <?php
 $homepage = file_get_contents('http://sensor.local:5000/brightness/');
 echo $homepage;
 ?>
-</br>
 
-<h2>Humidity</h2>
+<td/>
+<td>
+<h3>Humidity</h3>
 <?php
 $homepage = file_get_contents('http://sensor.local:5000/humidity/');
 echo $homepage;
 ?>
-</br>
 
-<h2>On-board Temperature</h2>
+<td/>
+<td>
+
+<h3>On-board Temperature</h3>
 <?php
 $homepage = file_get_contents('http://sensor.local:5000/onboard-temp/');
 echo $homepage;
 ?>
-</br>
 
-<h2>Temperature</h2>
+<td/>
+<tr>
+<td>
+
+<h3>Temperature</h3>
 <?php
 $homepage = file_get_contents('http://sensor.local:5000/temperature/');
 echo $homepage;
 ?>
-</br>
 
-<h2>Barometer</h2>
+<td/>
+<td>
+
+<h3>Barometer</h3>
 <?php
 $homepage = file_get_contents('http://sensor.local:5000/barometer/');
 echo $homepage;
 ?>
-</br>
 
+<td/>
+<td>
 
-<h2>Human sensor</h2>
+<h3>Human sensor</h3>
 <?php
 $homepage = file_get_contents('http://sensor.local:5000/human/');
 echo $homepage;
 ?>
-</br>
+
+<td/>
+<tr/>
+<table/>
 
 
 <h2>AirNow</h2>
+
+<table>
+<tr>
+<td>
+
 <iframe height="340" src="https://epa.gov/cgi-bin/schoolflagwidgetcurrentforecast.cgi?z=95037&n=AirNow" width="230"></iframe>
+
+<td/>
+<td>
 
 <h2>weatherwidget <h2/>
 <a class="weatherwidget-io" href="https://forecast7.com/en/37d13n121d65/morgan-hill/" data-label_1="MORGAN HILL" data-label_2="WEATHER" data-theme="original" >MORGAN HILL WEATHER</a>
@@ -66,6 +96,9 @@ echo $homepage;
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 </script>
 
+<td/>
+<tr/>
+<table/>
 <h2>alertscc</h2>
 
 
