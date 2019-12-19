b22c52d6333e09a9dd9db849f23417241010f668
mayelespino <mayel.espino@gmail.com>
2019-12-18 20:38:03 -0800

    Added atHome history.

--- sensor/sensor-history.py
+++ sensor/sensor-history.py
@@ -3,18 +3,25 @@ from datetime import datetime
 
 tempFileName = '/mnt/ramdisk/temperature.txt'
 tempHistoryFileName = '/mnt/ramdisk/temperature-history.txt'
+
+atHomeFileName = '/mnt/ramdisk/bluetooth-status.txt'
+atHomeHistoryFileName = '/mnt/ramdisk/bluetooth-history.txt'
+
 historySize = 24
 
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
+#
+#
+#
 fullTempLine = ""
 with open(tempFileName, 'r') as tf:
   temperature = tf.read()
   tokens = temperature.split()
   tempInt = int(tokens[0])
   tempLine = "-" * tempInt
-  fullTempLine = dt_string + "|" + temperature + "|" + tempLine + "|"
+  fullTempLine = dt_string + "_|_" + temperature + "_|" + tempLine + "+"
 
 if not os.path.exists(tempHistoryFileName):
     with open(tempHistoryFileName, 'w'): pass
@@ -32,3 +39,28 @@ if len(historyLines) > historySize:
 with open(tempHistoryFileName, 'w') as hf:
   for line in historyLines:
     hf.write(line+'\n')
+#
+#
+#
+fullAtHomeLine = ""
+with open(atHomeFileName, 'r') as hf:
+  atHome = hf.read()
+  fullAtHomeLine = dt_string + "=>" + atHome 
+
+if not os.path.exists(atHomeHistoryFileName):
+    with open(atHomeHistoryFileName, 'w'): pass
+
+historyLines = ""
+with open(atHomeHistoryFileName, 'r') as hf:
+  historyLines =  hf.readlines()
+  historyLines = [x.strip() for x in historyLines] 
+
+historyLines.append(fullAtHomeLine)
+
+if len(historyLines) > historySize:
+  historyLines = historyLines[1:]
+
+with open(atHomeHistoryFileName, 'w') as hf:
+  for line in historyLines:
+    hf.write(line+'\n')
+#
